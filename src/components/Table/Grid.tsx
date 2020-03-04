b7bc04b92ec1d3a30b3829ce92b6b9314aa5aab9
xsp0723 <768366139@qq.com>
2020-03-04 15:13:09 +0800

    模块修改

--- src/components/Table/Grid.tsx
+++ src/components/Table/Grid.tsx
@@ -60,7 +60,7 @@ const ResizeableTitle = (props: any) => {
 };
 
 interface IDataType{
-    uuid:string,
+    uuid?:string,
     [key:string]:any
 }
 
@@ -254,7 +254,7 @@ const Grid: IGird = function <T extends IDataType>(p: IGridProp<T> & TableProps<
     };
 
     const rowKey = (record: T, index?: number) => {
-        return record.uuid;
+        return   record.uuid?record.uuid:record.id;
     }
 
     config['rowKey'] = rowKey;
