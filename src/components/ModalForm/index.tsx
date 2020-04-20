ba41779557be922fc528989c1065ffbbf76f12fb
mathcw <964839421@qq.com>
2020-04-20 11:22:29 +0800

    form重构

--- src/components/ModalForm/index.tsx
+++ src/components/ModalForm/index.tsx
@@ -1,8 +1,9 @@
-import React, { useMemo } from "react";
+import React, { useState } from "react";
 import { Select, Input, DatePicker, TimePicker, Col, InputNumber, Button, Form } from "antd";
 import { getEnum } from "@/utils/enum";
 
 import styles from "./index.less";
+import moment from "moment";
 
 const { Option } = Select;
 
@@ -25,10 +26,28 @@ interface IModalForm {
   change?: (field: string, val: any, rst: any) => object;
 }
 
+interface IValidate{
+  [field:string]:{
+    validateStatus:any,
+    help:any
+  }
+}
+
 const initData = (list: IModalForm['list'], data: IModalForm['data']) => {
   const rst = {};
   Object.keys(list).forEach((field: string) => {
     rst[field] = data ? data[field] : '';
+  })
+  return rst;
+}
+
+const initValidate = (list: IModalForm['list']) =>{
+  const rst:IValidate = {};
+  Object.keys(list).forEach((field: string) => {
+    rst[field] = {
+      validateStatus:null,
+      help:null
+    };
   })
   return rst;
 }
@@ -39,11 +58,12 @@ const ModalForm: React.FC<IModalForm> = ({
   onSubmit,
   onCancel,
 }) => {
-  const [form] = Form.useForm();
-  const formData = useMemo(() => initData(list, ref), [list, ref])
+  const [data,setData] = useState<{}>(initData(list, ref));
+  const [validate,setValidate] = useState<IValidate>(initValidate(list));
 
-  const selectChange = (value: any, field: string) => {
+  const onChange = (value: any, field: string) => {
     const rst = {};
+    rst[field] = value;
     const clearCascade = (checkField: string) => {
       let nField = null;
       Object.keys(list).forEach((key) => {
@@ -56,20 +76,39 @@ const ModalForm: React.FC<IModalForm> = ({
     }
     let cField = clearCascade(field);
     while (cField) {
+      if(list[cField].required){
+        validate[cField] = {
+          validateStatus:'error',
+          help:`请输入${list[cField].text}`
+        }
+        setValidate({...validate})
+      }
       cField = clearCascade(cField);
     }
-    form.setFieldsValue(
-      { ...rst }
-    )
+    setData({...data,...rst});
+    if(list[field].required){
+      if(value === null ||value === undefined || value === '' || (Array.isArray(value) && value.length ===0)){
+        validate[field] = {
+          validateStatus:'error',
+          help:`请输入${list[field].text}`
+        }
+        setValidate({...validate})
+      }else{
+        validate[field] = {
+          validateStatus:null,
+          help:null
+        }
+        setValidate({...validate})
+      }
+    }
   }
 
   const renderArraySelect = (
     cfg: any,
     field: string,
-    row:any,
     disabled: boolean = false
   ) => {
-    const Enum = getEnum(cfg, row) || [];
+    const Enum = getEnum(cfg, data) || [];
     if (cfg.multi) {
       return <Select
         showSearch
@@ -77,6 +116,8 @@ const ModalForm: React.FC<IModalForm> = ({
         mode="multiple"
         getPopupContainer={node => node}
         disabled={disabled}
+        value={data[field]}
+        onChange={(value) => onChange(value, field)}
       >
         {Object.keys(Enum).map(key => (
           <Option key={key} value={Enum[key]}>
@@ -91,6 +132,7 @@ const ModalForm: React.FC<IModalForm> = ({
         optionFilterProp="children"
         getPopupContainer={node => node}
         disabled={disabled}
+        onChange={(value) => onChange(value, field)}
       >
         {Object.keys(Enum).map(key => (
           <Option key={key} value={Enum[key]}>
@@ -104,10 +146,9 @@ const ModalForm: React.FC<IModalForm> = ({
   const renderEnumSelect = (
     cfg: any,
     field: string,
-    row:any,
     disabled: boolean = false
   ) => {
-    const Enum = getEnum(cfg, row) || {};
+    const Enum = getEnum(cfg, data) || {};
     if (cfg.multi) {
       return (
         <Select
@@ -115,8 +156,9 @@ const ModalForm: React.FC<IModalForm> = ({
           optionFilterProp="children"
           mode="multiple"
           getPopupContainer={node => node}
-          onChange={(value) => selectChange(value, field)}
+          onChange={(value) => onChange(value, field)}
           disabled={disabled}
+          value={data[field]}
         >
           {Object.keys(Enum).map(key => (
             <Option key={key} value={key}>
@@ -132,7 +174,8 @@ const ModalForm: React.FC<IModalForm> = ({
         optionFilterProp="children"
         getPopupContainer={node => node}
         disabled={disabled}
-        onChange={(value) => selectChange(value, field)}
+        value={data[field]}
+        onChange={(value) => onChange(value, field)}
       >
         {Object.keys(Enum).map(key => (
           <Option key={key} value={key}>
@@ -159,6 +202,8 @@ const ModalForm: React.FC<IModalForm> = ({
             mode="multiple"
             getPopupContainer={node => node}
             disabled={disabled}
+            value={data[field]}
+            onChange={(value) => onChange(value, field)}
           >
             {Object.keys(Enum).map(key => (
               <Option key={key} value={key}>
@@ -174,6 +219,8 @@ const ModalForm: React.FC<IModalForm> = ({
           optionFilterProp="children"
           getPopupContainer={node => node}
           disabled={disabled}
+          value={data[field]}
+          onChange={(value) => onChange(value, field)}
         >
           {Object.keys(Enum).map(key => (
             <Option key={key} value={key}>
@@ -188,23 +235,24 @@ const ModalForm: React.FC<IModalForm> = ({
 
   const submit = () => {
     if (onSubmit) {
-      form.validateFields().then(
-        (formValues: any) => {
-          const rst = {};
-          Object.keys(list).forEach((field: string) => {
-            rst[field] = formValues[field];
-            if (list[field].type && list[field].type === 'date') {
-              rst[field] = formValues[field].format('YYYY-MM-DD')
-            }
-            if (list[field].type && list[field].type === 'time') {
-              rst[field] = formValues[field].format('HH:mm')
-            }
-          })
-          onSubmit(rst)
-        }, () => {
-
+      let check = true;
+      Object.keys(list).forEach((field: string) => {
+        if( check && validate[field].validateStatus == 'error'){
+          check = false;
         }
-      );
+        if( list[field].required && (data[field]===null || data[field] === undefined || data[field] ==='')){
+          check = false;
+          validate[field] = {
+            validateStatus:'error',
+            help:`请输入${list[field].text}`
+          }
+          setValidate({...validate})
+        }
+      })
+      if(!check){
+        return ;
+      }
+      onSubmit(data);
     }
   };
 
@@ -217,17 +265,16 @@ const ModalForm: React.FC<IModalForm> = ({
   return (
     <React.Fragment>
       <Col className={styles.ModalForm}>
-        <Form labelCol={{ span: 7 }} wrapperCol={{ span: 16 }} form={form}
-          onFinish={submit} initialValues={formData}>
+        <Form labelCol={{ span: 7 }} wrapperCol={{ span: 16 }}
+          onFinish={submit}>
           {Object.keys(list).map(field => (
             <React.Fragment key={field}>
               {list[field].editable === false && !list[field].type && (
                 <Form.Item
                   style={{ margin: "12px 0" }}
                   label={list[field].text}
-                  name={field}
                 >
-                  <Input readOnly />
+                  <Input value={data[field]} readOnly />
                 </Form.Item>
               )}
               {list[field].editable === false &&
@@ -238,9 +285,8 @@ const ModalForm: React.FC<IModalForm> = ({
                   <Form.Item
                     style={{ margin: "12px 0" }}
                     label={list[field].text}
-                    name={field}
                   >
-                    <Input readOnly />
+                    <Input value={data[field]} readOnly />
                   </Form.Item>
                 )}
               {list[field].editable === false &&
@@ -249,9 +295,8 @@ const ModalForm: React.FC<IModalForm> = ({
                   <Form.Item
                     style={{ margin: "12px 0" }}
                     label={list[field].text}
-                    name={field}
                   >
-                    {renderArraySelect(list[field], field,formData, true)}
+                    {renderArraySelect(list[field], field, true)}
                   </Form.Item>
                 )}
               {list[field].editable === false &&
@@ -260,7 +305,6 @@ const ModalForm: React.FC<IModalForm> = ({
                   <Form.Item
                     style={{ margin: "12px 0" }}
                     label={list[field].text}
-                    name={field}
                   >
                     {renderPairEditSelect(list[field], field)}
                   </Form.Item>
@@ -275,19 +319,21 @@ const ModalForm: React.FC<IModalForm> = ({
                   <Form.Item
                     style={{ margin: "12px 0" }}
                     label={list[field].text}
-                    name={field}
                   >
-                    {renderEnumSelect(list[field], field,formData, true)}
+                    {renderEnumSelect(list[field], field, true)}
                   </Form.Item>
                 )}
               {list[field].editable !== false && !list[field].type && (
                 <Form.Item
                   style={{ margin: "12px 0" }}
                   label={list[field].text}
-                  name={field}
-                  rules={[{ required: true, message: `请输入${list[field].text}` }]}
+                  required={list[field].required}
+                  validateStatus={validate[field].validateStatus}
+                  help={validate[field].help}
                 >
                   <Input
+                  value={data[field]}
+                  onChange={(e)=>onChange(e.target.value,field)}
                   />
                 </Form.Item>
               )}
@@ -295,22 +341,29 @@ const ModalForm: React.FC<IModalForm> = ({
                 <Form.Item
                   style={{ margin: "12px 0" }}
                   label={list[field].text}
-                  name={field}
-                  rules={[{ required: list[field].required, message: `请输入${list[field].text}` }]}
+                  required={list[field].required}
+                  validateStatus={validate[field].validateStatus}
+                  help={validate[field].help}
                 >
-                  <InputNumber />
+                  <InputNumber 
+                  value={data[field]}
+                  onChange={(v)=>onChange(v,field)}
+                  />
                 </Form.Item>
               )}
               {list[field].editable !== false && list[field].type === "date" && (
                 <Form.Item
                   style={{ margin: "12px 0" }}
                   label={list[field].text}
-                  name={field}
-                  rules={[{ required: list[field].required, message: `请输入${list[field].text}` }]}
+                  required={list[field].required}
+                  validateStatus={validate[field].validateStatus}
+                  help={validate[field].help}
                 >
                   <DatePicker
                     style={{ width: "100%" }}
                     format="YYYY-MM-DD"
+                    value={moment(data[field])}
+                    onChange={(date: moment.Moment | null, value: string)=>{onChange(value,field)}}
                   />
                 </Form.Item>
               )}
@@ -318,12 +371,15 @@ const ModalForm: React.FC<IModalForm> = ({
                 <Form.Item
                   style={{ margin: "12px 0" }}
                   label={list[field].text}
-                  name={field}
-                  rules={[{ required: list[field].required, message: `请输入${list[field].text}` }]}
+                  required={list[field].required}
+                  validateStatus={validate[field].validateStatus}
+                  help={validate[field].help}
                 >
                   <TimePicker
                     style={{ width: "100%" }}
                     format="HH:mm"
+                    value={moment(data[field])}
+                    onChange={(date: moment.Moment | null, value: string)=>{onChange(value,field)}}
                   />
                 </Form.Item>
               )}
@@ -332,10 +388,11 @@ const ModalForm: React.FC<IModalForm> = ({
                   <Form.Item
                     style={{ margin: "12px 0" }}
                     label={list[field].text}
-                    name={field}
-                    rules={[{ required: list[field].required, message: `请输入${list[field].text}` }]}
+                    required={list[field].required}
+                    validateStatus={validate[field].validateStatus}
+                    help={validate[field].help}
                   >
-                    {renderArraySelect(list[field], field,formData)}
+                    {renderArraySelect(list[field], field)}
                   </Form.Item>
                 )}
               {list[field].editable !== false &&
@@ -344,8 +401,9 @@ const ModalForm: React.FC<IModalForm> = ({
                   <Form.Item
                     style={{ margin: "12px 0" }}
                     label={list[field].text}
-                    name={field}
-                    rules={[{ required: list[field].required, message: `请输入${list[field].text}` }]}
+                    required={list[field].required}
+                    validateStatus={validate[field].validateStatus}
+                    help={validate[field].help}
                   >
                     {renderPairEditSelect(list[field], field)}
                   </Form.Item>
@@ -361,45 +419,13 @@ const ModalForm: React.FC<IModalForm> = ({
                 list[field].type !== "ArrayEdit" &&
                 list[field].type !== "PairEdit" && (
                   <Form.Item
-                    noStyle
-                    shouldUpdate={(prevValues, currentValues) => {
-                      //@ts-ignore
-                      return !!list[field].cascade && (prevValues[list[field].cascade] !== currentValues[list[field].cascade]);
-                    }}
-                  >
-                    {({ getFieldValue,getFieldsValue }) => {
-                      if (!list[field].cascade) {
-                        return (
-                          <Form.Item
-                            style={{ margin: "12px 0" }}
-                            label={list[field].text}
-                            name={field}
-                            rules={[{ required: list[field].required, message: `请输入${list[field].text}` }]}>
-                            {renderEnumSelect(list[field], field,formData)}
-                          </Form.Item>
-                        )
-                      }
-                      // @ts-ignore
-                      const shouldUpdate = getFieldValue(list[field].cascade) !== '' && getFieldValue(list[field].cascade) !== undefined && getFieldValue(list[field].cascade) !== null;
-                      const formInsData = {};
-                       // @ts-ignore
-                      formInsData[list[field].cascade] = getFieldValue(list[field].cascade)
-                      return shouldUpdate ? (
-                        <Form.Item
-                          style={{ margin: "12px 0" }}
-                          label={list[field].text}
-                          name={field}
-                          rules={[{ required: list[field].required, message: `请输入${list[field].text}` }]}>
-                          {renderEnumSelect(list[field],field,formInsData)}
-                        </Form.Item>
-                      ) : <Form.Item
-                        style={{ margin: "12px 0" }}
-                        label={list[field].text}
-                        name={field}
-                        rules={[{ required: list[field].required, message: `请输入${list[field].text}` }]}>
-                          {renderEnumSelect(list[field], field,formData)}
-                        </Form.Item>;
-                    }}
+                    style={{ margin: "12px 0" }}
+                    label={list[field].text}
+                    required={list[field].required}
+                    validateStatus={validate[field].validateStatus}
+                    help={validate[field].help}
+                    >
+                    {renderEnumSelect(list[field], field)}
                   </Form.Item>
                 )}
             </React.Fragment>
