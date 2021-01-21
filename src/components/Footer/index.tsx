e5e8f198f930e48613bfa977b1bf124243735c38
mathcw <964839421@qq.com>
2021-01-21 11:20:48 +0800

    icp

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,14 +11,14 @@ export default ({style}) =>{
         <Footer className={styles.footer} style={style?style:{}}>
             <div className={styles.link}>
                 <a
-                title='苏ICP备17030489号-3'
+                title='津ICP备2021000444号-1'
                 target='_blank'
                 href='http://beian.miit.gov.cn/'
                 >
-                苏ICP备17030489号-3 
+                津ICP备2021000444号-1
                 </a>
             </div>
-            <div>苏州方领圆冠网络科技有限公司</div>
+            <div>天津东青信息技术有限公司</div>
             <div className={styles.copyright}>
                 Copyright <CopyrightCircleFilled /> 2020 TourTool All Rights Reserved
             </div>
